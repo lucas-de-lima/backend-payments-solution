e4372c3d97fde310ec53ebf10b311c365efb2e6b
lucas-de-lima <lucasramosdelima@gmail.com>
2024-03-22 15:53:56 -0300

    feat: up project template

---
+++ src/main/kotlin/com/backend/backendpaymentssolution/BackendPaymentsSolutionApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.backendpaymentssolution
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendPaymentsSolutionApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendPaymentsSolutionApplication>(*args)
+}
---
+++ src/test/kotlin/com/backend/backendpaymentssolution/BackendPaymentsSolutionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.backendpaymentssolution
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendPaymentsSolutionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
